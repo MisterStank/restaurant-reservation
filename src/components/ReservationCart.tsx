eb20e723453addc2e4ba090f117bee25a2c8b846
MisterStank <chakkritchongkraichak@gmail.com>
2023-11-22 21:58:32 +0700

    Update ReservationCart.tsx

--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -2,6 +2,7 @@
 import { removeReservation } from "@/redux/features/cartSlice"
 import { useAppSelector } from "@/redux/store"
 import { AppDispatch } from "@/redux/store"
+import Link from "next/link"
 import { useDispatch } from "react-redux"
 
 export default function ReservationCart() {
@@ -15,12 +16,22 @@ export default function ReservationCart() {
                     <div className="text-xl">{ReservationItem.location}</div>
                     <div className="text-sm">Booking Date: {ReservationItem.reserveDate}</div>
                     <div className="text-sm">Number of guests: {ReservationItem.numOfGuests}</div>
+                    <div className="flex flex-row mt-5">
                     <button
-                        className="block rounded-full mt-5 bg-primary hover:bg-accent px-3 py-2
+                        className="block rounded-full bg-primary hover:bg-accent px-3 py-2
                         shadow-sm text-white" onClick={()=>dispatch(removeReservation(ReservationItem))}
                     >
                         Remove
                     </button>
+                    <Link href={`/restaurant`}>
+                    <button
+                        className="block rounded-full mx-1 bg-primary hover:bg-accent px-3 py-2
+                        shadow-sm text-white"
+                    >
+                        Edit
+                    </button>
+                    </Link>
+                    </div>
                 </div>
 
             ))
